729fce14c0da76394bbf53f1a6873795773b2b1e
Basinger <fel_simon@mail.ru>
2023-03-24 00:51:29 +0300

    Added Task002_ThreeNums

---
+++ Task002_ThreeNums/Program.cs
@@ -0,0 +1,21 @@
+﻿// Напишите программу, которая принимает на вход три числа и выдаёт максимальное из этих чисел.
+
+int a;
+Console.Write("Enter a number 'a': ");
+int.TryParse(Console.ReadLine(), out a);
+
+int b;
+Console.Write("Enter a number 'b': ");
+int.TryParse(Console.ReadLine(), out b);
+
+int c;
+Console.Write("Enter a number 'c': ");
+int.TryParse(Console.ReadLine(), out c);
+
+int max = a;
+
+if(a > max) max = a;
+if(b > max) max = b;
+if(c > max) max = c;
+
+Console.WriteLine("max = " + max);
