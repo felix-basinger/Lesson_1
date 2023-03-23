fe88c1baa1070911f8bdd0c8968604decd8b2f32
Basinger <fel_simon@mail.ru>
2023-03-24 00:35:53 +0300

    Added Test001_MinMaxNum

---
+++ Task001_MinMaxNum/Program.cs
@@ -0,0 +1,31 @@
+﻿// Напишите программу, которая на вход принимает два числа и выдаёт, какое число большее, а какое меньшее.
+
+int a;
+Console.Write("Enter a number 'a': ");
+int.TryParse(Console.ReadLine(), out a);
+
+int b;
+Console.Write("Enter a number 'b': ");
+int.TryParse(Console.ReadLine(), out b);
+
+int min = 0;
+int max = 0;
+
+if(a > b)
+{
+    max = a;
+    min = b;
+    Console.WriteLine("max number = " + max);
+    Console.WriteLine("min number = " + min);
+}
+else if(a < b)
+{
+    max = b;
+    min = a;
+    Console.WriteLine("max number = " + max);
+    Console.WriteLine("min number = " + min);
+}
+else
+{
+    Console.WriteLine(a + " = " + b);
+}
