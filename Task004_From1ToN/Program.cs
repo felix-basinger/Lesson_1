bd6e2db5aa3ef42336cbf742f06b0ca2d057ee8b
Basinger <fel_simon@mail.ru>
2023-03-24 01:51:25 +0300

    Added Task004_From1ToN

---
+++ Task004_From1ToN/Program.cs
@@ -0,0 +1,11 @@
+﻿// Напишите программу, которая на вход принимает число (N), а на выходе показывает все чётные числа от 1 до N.
+
+int N;
+Console.Write("Enter the number N: ");
+int.TryParse(Console.ReadLine(), out N);
+
+for(int i = 2; i <= N; i += 2)
+{
+    Console.Write(i);
+    Console.Write(", ");
+}
