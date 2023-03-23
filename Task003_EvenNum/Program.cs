630a0a837cd4fd622d6546eb6547e409ba3f3419
Basinger <fel_simon@mail.ru>
2023-03-24 01:11:01 +0300

    Added Task003_EvenNum

---
+++ Task003_EvenNum/Program.cs
@@ -0,0 +1,14 @@
+﻿// Напишите программу, которая на вход принимает число и выдаёт, является ли число чётным (делится ли оно на два без остатка).
+
+int num;
+Console.Write("Enter a number: ");
+int.TryParse(Console.ReadLine(), out num);
+
+if(num % 2 == 0)
+{
+    Console.WriteLine("Yes, it's even number.");
+}
+else
+{
+    Console.WriteLine("No, it's not even number.");
+}
